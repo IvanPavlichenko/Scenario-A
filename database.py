9cc7629e94a6355ec727149bef521f0667ba35b5
Ivan Pavlichenko <pavlichenko.ivan97@ya.ru>
2023-06-22 16:03:05 -0400

    and calculater

--- database.py
+++ database.py
@@ -1,3 +1,9 @@
 print("Hello salama")
 for i in range(1, 10 + 1):
     print(i)
+
+x = int(input("Давай ка число мне тут напиши, я тебе таблицу умножения до этого чилса сделаю: "))
+
+for r in range(1, x + 1):
+    for e in range(1, x + 1):
+        print(r, "*", e, "=", r * e)
